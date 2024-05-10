de4cf4a0d1df36ba855c872c660339a881f07997
Traister101 <66151907+Traister101@users.noreply.github.com>
2024-05-09 18:58:02 -0700

    Fix shift click item transferring

--- src/main/java/mod/traister101/sacks/common/menu/SackMenu.java
+++ src/main/java/mod/traister101/sacks/common/menu/SackMenu.java
@@ -28,6 +28,10 @@ public class SackMenu extends AbstractContainerMenu {
 	protected final int heldItemIndex;
 	protected final IItemHandler handler;
 	/**
+	 * The amount of slots this container has
+	 */
+	protected final int containerSlots;
+	/**
 	 * Index into the slot for the hotbar slot. Hotbar is at the end of the inventory.
 	 */
 	protected int itemIndex;
@@ -38,9 +42,10 @@ public class SackMenu extends AbstractContainerMenu {
 		this.hand = hand;
 		this.heldItemIndex = hotbarSlot;
 		this.handler = handler;
+		this.containerSlots = this.handler.getSlots();
 
 		if (this.hand == InteractionHand.MAIN_HAND) {
-			this.itemIndex = this.handler.getSlots() + SackMenu.this.heldItemIndex + 27;
+			this.itemIndex = containerSlots + heldItemIndex + 27;
 		} else {
 			this.itemIndex = -100;
 		}
@@ -59,30 +64,21 @@ public class SackMenu extends AbstractContainerMenu {
 	@Override
 	public ItemStack quickMoveStack(final Player player, final int slotIndex) {
 		final Slot slot = slots.get(slotIndex);
-		// Only move an item when the index clicked has any contents
+
 		if (slot.hasItem()) {
-			// The item in the current slot
 			final ItemStack slotStack = slot.getItem();
-			// The original amount in the slot
-			final ItemStack original = slotStack.copy();
-			if (moveStack(slotStack, slotIndex)) {
-				return ItemStack.EMPTY;
-			}
 
-			if (slotStack.getCount() == original.getCount()) {
-				return ItemStack.EMPTY;
-			}
+			if (slotIndex < containerSlots) {
+				if (!moveItemStackTo(slotStack, containerSlots, slots.size(), true)) return ItemStack.EMPTY;
+			} else if (!moveItemStackTo(slotStack, 0, containerSlots, false)) return ItemStack.EMPTY;
 
-			// Handle updates
 			if (slotStack.isEmpty()) {
-				slot.set(ItemStack.EMPTY);
-			} else {
-				slot.setChanged();
-			}
+				slot.setByPlayer(ItemStack.EMPTY);
+			} else slot.setChanged();
 
-			slot.onTake(this.player, slotStack);
-			return original;
+			return slotStack;
 		}
+
 		return ItemStack.EMPTY;
 	}
 
@@ -97,6 +93,8 @@ public class SackMenu extends AbstractContainerMenu {
 		if (dragType == 40 && clickType == ClickType.SWAP && hand == InteractionHand.OFF_HAND) return;
 		if (clickType == ClickType.PICKUP_ALL) return;
 
+		// TODO re-impliment this from 1.12
+		//  Yeah... it's still needed :|
 		super.clicked(slotIndex, dragType, clickType, player);
 	}
 
@@ -105,27 +103,118 @@ public class SackMenu extends AbstractContainerMenu {
 		return !(hand == InteractionHand.MAIN_HAND ? slots.get(itemIndex).getItem() : this.player.getOffhandItem()).isEmpty();
 	}
 
-	protected boolean moveStack(ItemStack stack, int slotIndex) {
-		return switch (IndexType.typeOf(slotIndex, handler.getSlots())) {
-			case CONTAINER -> true;
-			case HOTBAR -> !moveItemStackTo(stack, handler.getSlots(), handler.getSlots() + 27, false);
-			case MAIN_INVENTORY -> !moveItemStackTo(stack, handler.getSlots() + 27, handler.getSlots() + 36, false);
-		};
+	@Override
+	protected boolean moveItemStackTo(final ItemStack movedStack, final int startIndex, final int endIndex, final boolean reverseDirection) {
+		boolean haveMovedStack = false;
+		// Start iterating from the end if we merge in reverse order
+		int slotIndex = reverseDirection ? endIndex - 1 : startIndex;
+
+		if (movedStack.isStackable()) {
+			while (!movedStack.isEmpty()) {
+				if (reverseDirection) {
+					if (slotIndex < startIndex) break;
+				} else if (slotIndex >= endIndex) break;
+
+				final Slot slot = this.slots.get(slotIndex);
+				final ItemStack slotStack = slot.getItem();
+
+				// Can't merge these stacks
+				if (!ItemStack.isSameItemSameTags(movedStack, slotStack)) {
+					slotIndex += (reverseDirection) ? -1 : 1;
+					continue;
+				}
+
+				final int total = slotStack.getCount() + movedStack.getCount();
+
+				final int maxSize;
+				// If it's our container slots use the slot limit to determine the max stack size
+				if (slotIndex < containerSlots) {
+					maxSize = slot.getMaxStackSize();
+				} else maxSize = Math.min(slot.getMaxStackSize(), movedStack.getMaxStackSize());
+
+				// Can fully consume the merge stack
+				if (maxSize >= total) {
+					movedStack.setCount(0);
+					slotStack.setCount(total);
+					slot.setChanged();
+					haveMovedStack = true;
+					slotIndex += (reverseDirection) ? -1 : 1;
+					continue;
+				}
+
+				// Can only partially consume the stack
+				if (maxSize > slotStack.getCount()) {
+					movedStack.shrink(maxSize - slotStack.getCount());
+					slotStack.grow(maxSize - slotStack.getCount());
+					slot.setChanged();
+					haveMovedStack = true;
+					slotIndex += (reverseDirection) ? -1 : 1;
+					continue;
+				}
+				slotIndex += (reverseDirection) ? -1 : 1;
+			}
+		}
+
+		// Try and fill empty slots now
+		if (!movedStack.isEmpty()) {
+			if (reverseDirection) {
+				slotIndex = endIndex - 1;
+			} else slotIndex = startIndex;
+
+			while (true) {
+				if (reverseDirection) {
+					if (slotIndex < startIndex) break;
+				} else if (slotIndex >= endIndex) break;
+
+				final Slot slot = this.slots.get(slotIndex);
+
+				// Continue early if we can't put anything in this slot
+				if (slot.hasItem() || !slot.mayPlace(movedStack)) {
+					slotIndex += (reverseDirection) ? -1 : 1;
+					continue;
+				}
+
+				// If it's our container slots use the slots stack cap
+				if (slotIndex < containerSlots) {
+					slot.setByPlayer(movedStack.split(slot.getMaxStackSize()));
+					haveMovedStack = true;
+					slotIndex += (reverseDirection) ? -1 : 1;
+					continue;
+				}
+
+				{
+					final int splitSize = Math.min(slot.getMaxStackSize(movedStack), movedStack.getMaxStackSize());
+					// Can merge
+					if (movedStack.getCount() > splitSize) {
+						slot.setByPlayer(movedStack.split(splitSize));
+						haveMovedStack = true;
+						slotIndex += (reverseDirection) ? -1 : 1;
+						continue;
+					}
+				}
+
+				// Put the whole stack in the slot
+				slot.setByPlayer(movedStack.split(movedStack.getCount()));
+				haveMovedStack = true;
+				slotIndex += (reverseDirection) ? -1 : 1;
+			}
+		}
+		return haveMovedStack;
 	}
 
 	/**
 	 * Adds the slots for this container
 	 */
 	protected void addContainerSlots() {
-		switch (handler.getSlots()) {
+		switch (containerSlots) {
 			case 1 -> addSlots(1, 1, 80, 32);
 			case 4 -> addSlots(2, 2, 71, 23);
 			case 8 -> addSlots(2, 4, 53, 23);
 			case 18 -> addSlots(2, 9, 8, 34);
 			default -> {
 				// We want to round up, integer math rounds down
-				final int rows = (int) Math.ceil((double) handler.getSlots() / 9);
-				final int columns = handler.getSlots() / rows;
+				final int rows = (int) Math.ceil((double) containerSlots / 9);
+				final int columns = containerSlots / rows;
 				addSlots(rows, columns);
 			}
 		}
@@ -181,22 +270,6 @@ public class SackMenu extends AbstractContainerMenu {
 		// Hotbar. Indexes [27, 36)
 		for (int k = 0; k < 9; k++) {
 			addSlot(new Slot(inventory, k, 8 + k * 18, 142));
-		}
-	}
-
-
-	public enum IndexType {
-		CONTAINER,
-		MAIN_INVENTORY,
-		HOTBAR;
-
-		public static IndexType typeOf(final int slotIndex, final int containerSlots) {
-			if (slotIndex < containerSlots) {
-				return CONTAINER;
-			} else if (slotIndex < containerSlots + 27) {
-				return MAIN_INVENTORY;
-			}
-			return HOTBAR;
 		}
 	}
 }
