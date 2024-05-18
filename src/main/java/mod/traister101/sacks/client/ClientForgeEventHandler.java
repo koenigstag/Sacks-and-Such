1bb1dc2d5745bb7cc401cdaadb1043db23ccdb75
Traister101 <66151907+Traister101@users.noreply.github.com>
2024-05-17 23:53:44 -0700

    Pick block with sacks tries to find a stack like vanilla, if it cannot then we handle it.

--- src/main/java/mod/traister101/sacks/client/ClientForgeEventHandler.java
+++ src/main/java/mod/traister101/sacks/client/ClientForgeEventHandler.java
@@ -7,7 +7,21 @@ import mod.traister101.sacks.util.SNSUtils.ToggleType;
 import mod.traister101.sacks.util.handlers.PickBlockHandler;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.HitResult.Type;
 
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -54,7 +68,71 @@ public final class ClientForgeEventHandler {
 			// In creative so don't handle
 			if (MC.player.isCreative()) return;
 
-			event.setCanceled(PickBlockHandler.onPickBlock(MC.player, MC.hitResult));
+			event.setCanceled(
+					vanillaPickBlock(MC.hitResult, MC.player, MC.level, MC.gameMode) || PickBlockHandler.onPickBlock(MC.player, MC.hitResult));
 		}
+	}
+
+	/**
+	 * Copy of vanillas client pick block logic for us to call
+	 *
+	 * @return If the vanilla handling succeeded
+	 */
+	public static boolean vanillaPickBlock(final HitResult hitResult, final Player player, final Level level, final MultiPlayerGameMode gameMode) {
+		final boolean creative = player.getAbilities().instabuild;
+
+		final ItemStack pickedStack = getPickedStack(player, level, hitResult, creative);
+
+		if (pickedStack.isEmpty()) return false;
+
+		final Inventory inventory = player.getInventory();
+
+		if (creative) {
+			inventory.setPickedItem(pickedStack);
+			gameMode.handleCreativeModeItemAdd(player.getItemInHand(InteractionHand.MAIN_HAND), Inventory.INVENTORY_SIZE + inventory.selected);
+			return true;
+		}
+
+		final int slotIndex = inventory.findSlotMatchingItem(pickedStack);
+		if (slotIndex != Inventory.NOT_FOUND_INDEX) {
+			if (Inventory.isHotbarSlot(slotIndex)) {
+				inventory.selected = slotIndex;
+			} else {
+				gameMode.handlePickItem(slotIndex);
+			}
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * @return The picked {@link ItemStack}. {@link ItemStack#EMPTY} for when nothing should be added
+	 */
+	private static ItemStack getPickedStack(final Player player, final Level level, final HitResult hitResult, final boolean creative) {
+		final Type type = hitResult.getType();
+
+		if (type == Type.BLOCK) {
+			final BlockPos blockpos = ((BlockHitResult) hitResult).getBlockPos();
+			final BlockState blockState = level.getBlockState(blockpos);
+			if (blockState.isAir()) return ItemStack.EMPTY;
+
+			final ItemStack itemStack = blockState.getCloneItemStack(hitResult, level, blockpos, player);
+
+			if (creative && Screen.hasControlDown() && blockState.hasBlockEntity()) {
+				final BlockEntity blockEntity = level.getBlockEntity(blockpos);
+				if (blockEntity != null) {
+					MC.addCustomNbtData(itemStack, blockEntity);
+				}
+			}
+
+			return itemStack;
+		}
+
+		if (type != Type.ENTITY || !creative) return ItemStack.EMPTY;
+
+		final Entity entity = ((EntityHitResult) hitResult).getEntity();
+
+		return entity.getPickedResult(hitResult);
 	}
 }
