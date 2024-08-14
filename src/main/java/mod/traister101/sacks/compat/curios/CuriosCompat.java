736ed3bfae8d79d8c2b6935f486b420909c7fbff
Traister101 <66151907+Traister101@users.noreply.github.com>
2024-08-14 15:01:03 -0700

    Disable curios renderers

--- src/main/java/mod/traister101/sacks/compat/curios/CuriosCompat.java
+++ src/main/java/mod/traister101/sacks/compat/curios/CuriosCompat.java
@@ -1,15 +1,11 @@
 package mod.traister101.sacks.compat.curios;
 
-import top.theillusivec4.curios.api.client.CuriosRendererRegistry;
-
-import mod.traister101.sacks.client.renderer.CurioSackItemRenderer;
-import mod.traister101.sacks.common.items.*;
-
 public final class CuriosCompat {
 
 	public static void clientSetup() {
-		for (final DefaultContainers sackType : DefaultContainers.values()) {
-			CuriosRendererRegistry.register(SNSItems.ITEM_CONTAINERS.get(sackType).get(), CurioSackItemRenderer::new);
-		}
+		// TODO do renderers later
+//		for (final DefaultContainers sackType : DefaultContainers.values()) {
+//			CuriosRendererRegistry.register(SNSItems.ITEM_CONTAINERS.get(sackType).get(), CurioSackItemRenderer::new);
+//		}
 	}
 }
